7113ec3d5a00eb2177a0e0fa984e8b7175072fa6
justanwar <42809091+justanwar@users.noreply.github.com>
2025-11-17 22:00:24 +0800

    Fix typo

--- lib/app_config/app_config.dart
+++ lib/app_config/app_config.dart
@@ -155,11 +155,6 @@ const List<String> appWalletOnlyAssetList = [
 /// This will not affect existing wallets.
 /// Reduced to only KMD to minimize initial connections and resource usage.
 List<String> get enabledByDefaultCoins => [
-<<<<<<< HEAD
-  'KMD', // Komodo ecosystem coin
-  'BTC-segwit' // Default Fiat Ramps coin
-];
-=======
       'BTC-segwit',
       'FIRO',
       'USDT-PLG20',
@@ -173,7 +168,6 @@ List<String> get enabledByDefaultCoins => [
       if (kDebugMode) 'DOC',
       if (kDebugMode) 'MARTY',
     ];
->>>>>>> a0a78857f (Set FIRO to default)
 
 const String logsDbName = 'logs';
 const String appFolder = 'KomodoWallet';
